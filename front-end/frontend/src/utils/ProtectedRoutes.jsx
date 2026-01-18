a7d780ce7a2381dde21cca9340c8fa85c9c63b66
Austin Alberd <austinalberd2005@gmail.com>
2026-01-18 15:13:35 -0600

    created authenticated route with all nessicary logic

--- front-end/frontend/src/utils/ProtectedRoutes.jsx
+++ front-end/frontend/src/utils/ProtectedRoutes.jsx
@@ -1,12 +1,28 @@
 import { Outlet, Navigate } from "react-router-dom"
+import { useEffect, useState } from 'react'
+
 
 const ProtectedRoutes = () =>{
-    if (cookies.access_token){
-        console.log(cookies)
-        return <Outlet/>
-    }else{
-        return <Navigate to="/"/>
-    }
+    const [isAuth,setIsAuth] = useState(null)
+
+    useEffect(()=>{
+        const check_session = async () =>{
+            try{
+                const auth_status = await fetch("http://localhost:3000/authenticate/validate-cookie",{
+                    method: "POST",
+                    credentials:"include"
+                })
+
+                setIsAuth(auth_status.status === 200)
+            }catch(e){
+                setIsAuth(false)
+            }
+        }
+        check_session()
+    },[])
+    
+    if(isAuth === null) return <p>Loading.....</p>
+    return isAuth ? <Outlet/> : <Navigate to="/"/>
 }
 
 export default ProtectedRoutes
