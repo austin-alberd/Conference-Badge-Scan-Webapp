64ec7b7e31a71c90c6a787a6db945606bf2b165f
Austin Alberd <austinalberd2005@gmail.com>
2026-01-19 16:56:29 -0600

    added in all data that needs to be rendered into the user home page

--- front-end/frontend/src/pages/userhome.jsx
+++ front-end/frontend/src/pages/userhome.jsx
@@ -4,50 +4,41 @@ import "./userhome.css"
 function UserHome(){
     const [firstName, setFirstName] = useState("NoName")
     const [points, setPoints] = useState("0")
-    const [leaderboard, setLeaderBoard] = useState()
+    const [leaderboard, setLeaderBoard] = useState([])
 
     useEffect(()=>{
-        const fill_data = async ()=>{
-            const user_data = await fetch("http://localhost:3000/user",{
-                method:"GET",
-                credentials:"include"
-            }).then(response =>{
-                if(!response.ok){throw new Error("Error" + response.statusText)}
-                return response.json()
-            }).then(data =>{
-                setFirstName(data.first_name)
-            })
-            
-            const points = await fetch("http://localhost:3000/points",{
-                method:"GET",
-                credentials:"include"
-            }).then(response =>{
-                if(!response.ok){throw new Error("Error" + response.statusText)}
-                return response.json()
-            }).then(data =>{
-                setPoints(data.pointTotal)
-            })
+            const get_data = async ()=>{
+                const res = await fetch("http://localhost:3000/user-home-data",{
+                    method:"GET",
+                    credentials:"include"
+                }).then(response =>{
+                    if(!response.ok){
+                        throw new Error('Network response was not ok: ' + response.statusText)
+                    }
 
-            const leaderboard = await fetch("http://localhost:3000/leaderboard",{
-                method:"GET",
-                credentials:"include"
-            }).then(response =>{
-                if(!response.ok){throw new Error("Error" + response.statusText)}
-                return response.json()
-            }).then(data =>{
-                setLeaderBoard(data)
-            })
-
-
-        }
-
-        fill_data()
+                    return response.json()
+                }).then(data =>{
+                    console.log(data)
+                    setFirstName(data.firstName)
+                    setPoints(data.pointTotal)
+                    setLeaderBoard(data.leaderboard)
+                })
+            }
+            get_data()
     },[])
 
     return(
         <>
             <h1>Hello {firstName}</h1>
             <p>You currently have {points} points</p>
+            <h2>Leaderboard</h2>
+            <ul>
+                {leaderboard.map(item=>(
+                    <li key={item.username}>
+                        {item.username} - {item.point_total}
+                    </li>
+                ))}
+            </ul>
         </>
     )
 }
